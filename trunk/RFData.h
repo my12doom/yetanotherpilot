3dc216112e606a1a86946e9bf5afc59954684430
my12doom2@gmail.com <my12doom2@gmail.com@c30ad9b1-c497-8ffe-a940-f6b3f6b8f5c0>
2013-09-03 09:45:51 +0000

    implemented data sending back from pilot and storing on ground station.

--- trunk/RFData.h
+++ trunk/RFData.h
@@ -18,6 +18,14 @@ typedef struct
 	int pressure;					// 4 byte
 } imu_data;
 
+typedef struct
+{
+	unsigned char fly_mode;					// 1 byte
+	short error[3];							// 6 byte, unit base: 0.01 degree, range: -18000 ~ 18000
+	short target[3];						// 6 byte, unit base: 0.01 degree, range: -18000 ~ 18000
+	int altitude;							// 4 byte, unit base: 0.01 meter relative to launch ground.
+	short rc[3];							// 6 byte, roll, pitch, yaw channel, switch is ignored.
+} pilot_data;
 
 typedef struct
 {
@@ -26,11 +34,13 @@ typedef struct
 	{
 		sensor_data sensor;	// 20 byte
 		imu_data imu;		// 24 byte
+		pilot_data pilot;	// 23 byte
 	}data;
 } rf_data;
 
 #define TAG_SENSOR_DATA	0x1200000000000000
 #define TAG_IMU_DATA	0x8700000000000000
+#define TAG_PILOT_DATA	0x6500000000000000
 #define TAG_MASK		0xff00000000000000
 
 #endif
