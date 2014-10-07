93f09512f351faa34fff877c077433cc7ec5dd13
my12doom2@gmail.com <my12doom2@gmail.com@c30ad9b1-c497-8ffe-a940-f6b3f6b8f5c0>
2014-10-07 16:00:03 +0000

    new dma based UART4 telemetry.

--- trunk/common/uart4.c
+++ trunk/common/uart4.c
@@ -14,6 +14,8 @@ static int end_sentence = 0;				// valid sentence length in circular buffer
 static char tx_buffer[GPS_BUFFER_BLOCK];
 static int tx_start = 0;					// valid data start in circular
 static int tx_end = 0;						// valid data length in circular buffer
+static int ongoing_tx_size = 0;
+static int dma_running = 0;
 
 void UART4_IRQHandler(void)
 {
@@ -33,6 +35,63 @@ void UART4_IRQHandler(void)
 			end_sentence = end;
 		buffer[end] = 0;
 	}
+}
+
+int UART4_Send(void *buf, int size)
+{
+	int i;
+	const char *p = (const char*)buf;
+	if ((tx_end + size) > sizeof(tx_buffer) && ((tx_end + size)%sizeof(tx_buffer))>= tx_start)
+		return 0;		// reject all data if buffer overrun
+
+	for(i=0; i<size; i++)
+		tx_buffer[(tx_end+i)%sizeof(tx_buffer)] = p[i];
+
+	tx_end = (tx_end+size)%sizeof(tx_buffer);
+
+	dma_handle_queue();
+	
+	return size;
+}
+
+
+static int dma_init()
+{
+	DMA_InitTypeDef DMA_InitStructure = {0};
+	NVIC_InitTypeDef NVIC_InitStructure;
+	
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA1, ENABLE);
+
+	NVIC_InitStructure.NVIC_IRQChannel = DMA1_Stream4_IRQn;  
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 3;  
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+
+
+	DMA_DeInit(DMA1_Stream4);  
+
+	DMA_InitStructure.DMA_Channel = DMA_Channel_4; 
+	DMA_InitStructure.DMA_PeripheralBaseAddr = (uint32_t)(&(UART4->DR));
+	DMA_InitStructure.DMA_Memory0BaseAddr = (uint32_t)&tx_buffer;
+	DMA_InitStructure.DMA_DIR = DMA_DIR_MemoryToPeripheral;
+	DMA_InitStructure.DMA_BufferSize = sizeof(tx_buffer);
+	DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+	DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+	DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+	DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+	DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
+	DMA_InitStructure.DMA_Priority = DMA_Priority_VeryHigh;
+	DMA_InitStructure.DMA_FIFOMode = DMA_FIFOMode_Disable;
+	DMA_InitStructure.DMA_FIFOThreshold = DMA_FIFOThreshold_HalfFull; 
+	DMA_InitStructure.DMA_MemoryBurst = DMA_MemoryBurst_Single; 
+	DMA_InitStructure.DMA_PeripheralBurst = DMA_PeripheralBurst_Single; 
+	DMA_Init(DMA1_Stream4, &DMA_InitStructure);
+	DMA_ITConfig(DMA1_Stream4,DMA_IT_TC,ENABLE);
+
+	USART_DMACmd(UART4, USART_DMAReq_Tx, ENABLE); 
+	
+ 	return 0;
 }
 
 void UART4_Init(uint32_t baud_rate)
@@ -76,8 +135,44 @@ void UART4_Init(uint32_t baud_rate)
 	USART_Init(UART4, &USART_InitStructure); 
 	USART_Cmd(UART4, ENABLE);
 	USART_ITConfig(UART4, USART_IT_RXNE, ENABLE);
+
+	// 
+	dma_init();
 }
 
+int dma_handle_queue()
+{
+	if (dma_running)
+		return 0;
+
+	DMA_Cmd(DMA1_Stream4, DISABLE);
+
+	ongoing_tx_size = tx_end - tx_start;
+	if (ongoing_tx_size == 0)
+		return 0;
+	if (ongoing_tx_size < 0)
+		ongoing_tx_size = sizeof(tx_buffer) - tx_start;
+
+
+	DMA1_Stream4->NDTR = ongoing_tx_size;
+	DMA1_Stream4->M0AR = (uint32_t)tx_buffer + tx_start;
+
+	DMA_Cmd(DMA1_Stream4, ENABLE);
+
+	dma_running = 1;
+
+	return 0;
+}
+
+void DMA1_Stream4_IRQHandler()
+{
+	tx_start = (tx_start + ongoing_tx_size) % sizeof(tx_buffer);
+	DMA_ClearFlag(DMA1_Stream4, DMA_FLAG_TCIF4);
+	dma_running = 0;
+	dma_handle_queue();
+}
+
+extern int parse_command_line(const char *line, char *out);
 static int parse_command(char *cmd)
 {
 	int response_size;
