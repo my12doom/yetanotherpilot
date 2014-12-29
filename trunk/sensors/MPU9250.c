07f36cc99aedad0df5cb2dd7c12654af8694e019
my12doom2@gmail.com <my12doom2@gmail.com@c30ad9b1-c497-8ffe-a940-f6b3f6b8f5c0>
2014-12-29 13:43:14 +0000

    driver for mpu9250 & adis16405

--- trunk/sensors/MPU9250.c
+++ trunk/sensors/MPU9250.c
@@ -1,16 +1,16 @@
-#include "MPU6050.h"
+#include "MPU9250.h"
 #include <stdio.h>
 #include "../common/I2C.h"
 #include "../common/common.h"
 #include "../common/printf.h"
 #include "../common/timer.h"
 
-int MPU6050SlaveAddress = 0xD2;
-#define MPU6050_REG_WHO_AM_I 0x75
+int MPU9250SlaveAddress = 0xD2;
+#define MPU9250_REG_WHO_AM_I 0x75
 
 // Gyro and accelerator registers
 #define	SMPLRT_DIV		0x19	//??????,???:0x07(125Hz)
-#define	MPU6050_CONFIG 0x1A	//??????,???:0x06(5Hz)
+#define	MPU9250_CONFIG 0x1A	//??????,???:0x06(5Hz)
 #define	GYRO_CONFIG		0x1B	//??????????,???:0x18(???,2000deg/s)
 #define	ACCEL_CONFIG	0x1C	//?????????????????,???:0x01(???,2G,5Hz)
 #define	ACCEL_XOUT_H	0x3B
@@ -33,40 +33,30 @@ int MPU6050SlaveAddress = 0xD2;
 
 static int res;
 // call initI2C before this
-int init_MPU6050(void)
+int init_MPU9250(void)
 {
 	uint8_t who_am_i = 0;
 	
-	res = I2C_ReadReg(MPU6050SlaveAddress, WHO_AM_I, &who_am_i, 1);
-	if (who_am_i != 0x68)
-		MPU6050SlaveAddress = 0xD0;
-
-	TRACE("start MPU6050\r\n");
+	TRACE("start MPU9250\r\n");
 	delayms(10);
-	I2C_WriteReg(MPU6050SlaveAddress, PWR_MGMT_1, 0x80);
+	I2C_WriteReg(MPU9250SlaveAddress, PWR_MGMT_1, 0x80);
 	delayms(10);
-	I2C_WriteReg(MPU6050SlaveAddress, PWR_MGMT_1, 0x00);
+	I2C_WriteReg(MPU9250SlaveAddress, PWR_MGMT_1, 0x00);
 	#if QUADCOPTER == 1
-	I2C_WriteReg(MPU6050SlaveAddress, SMPLRT_DIV, 0x01);
+	I2C_WriteReg(MPU9250SlaveAddress, SMPLRT_DIV, 0x01);
 	#else
-	I2C_WriteReg(MPU6050SlaveAddress, SMPLRT_DIV, 0x07);
+	I2C_WriteReg(MPU9250SlaveAddress, SMPLRT_DIV, 0x07);
 	#endif
-	I2C_WriteReg(MPU6050SlaveAddress, MPU6050_CONFIG, 0x2);
-	I2C_WriteReg(MPU6050SlaveAddress, GYRO_CONFIG, 1 << 3);			// full scale : 500 degree/s, ~65.5 LSB/degree/s
-	I2C_WriteReg(MPU6050SlaveAddress, ACCEL_CONFIG, 0x18);			// full scale : 16g, 2048 = 1g
+	I2C_WriteReg(MPU9250SlaveAddress, MPU9250_CONFIG, 0x2);
+	I2C_WriteReg(MPU9250SlaveAddress, GYRO_CONFIG, 1 << 3);			// full scale : 500 degree/s, ~65.5 LSB/degree/s
+	I2C_WriteReg(MPU9250SlaveAddress, ACCEL_CONFIG, 0x18);			// full scale : 16g, 2048 = 1g
 	
-	res = I2C_ReadReg(MPU6050SlaveAddress, WHO_AM_I, &who_am_i, 1);
-	ERROR("MPU6050 initialized, WHO_AM_I=%x, address = %x\r\n", who_am_i, MPU6050SlaveAddress);
+	res = I2C_ReadReg(MPU9250SlaveAddress, WHO_AM_I, &who_am_i, 1);
+	ERROR("MPU9250 initialized, WHO_AM_I=%x, address = %x\r\n", who_am_i, MPU9250SlaveAddress);
 	
-	// enable I2C bypass for AUX I2C and initialize HMC5883 into continues mode
-#ifdef EXTERNAL_HMC5883_2
- 	I2C_WriteReg(MPU6050SlaveAddress, 0x37, 0);
-#else
-	I2C_WriteReg(MPU6050SlaveAddress, 0x37, 0x02);
-#endif
 	delayms(10);
 
-	if (who_am_i != 0x68)
+	if (who_am_i != 0x71)
 		return -1;
 	
 	return 0;
@@ -77,11 +67,11 @@ int init_MPU6050(void)
 static short gyro_o[3];
 static short gyro_raw[3];
 static int64_t lastus = -1;
-int read_MPU6050(short*data)
+int read_MPU9250(short*data)
 {	
 	int i;
 	//int64_t us;
-	int result = I2C_ReadReg(MPU6050SlaveAddress, ACCEL_XOUT_H, (uint8_t*)data, 14);
+	int result = I2C_ReadReg(MPU9250SlaveAddress, ACCEL_XOUT_H, (uint8_t*)data, 14);
 	for(i=0; i<7; i++)
 		swap((uint8_t*)&data[i], 2);
 
